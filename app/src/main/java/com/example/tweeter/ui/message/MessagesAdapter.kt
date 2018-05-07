e3e305c545925d19a9ddc1ae7bb169f9ff45ce9e
Huu Tai <taiem92dn@gmail.com>
2018-05-07 23:18:00 +0700

    - update UI

--- app/src/main/java/com/example/tweeter/ui/message/MessagesAdapter.kt
+++ app/src/main/java/com/example/tweeter/ui/message/MessagesAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.tweeter.ui.message
 
-import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.support.v7.widget.RecyclerView
@@ -11,7 +10,6 @@ import android.view.ViewGroup
 import com.example.tweeter.R
 import com.example.tweeter.util.Utils
 import kotlinx.android.synthetic.main.item_message.view.*
-import kotlin.collections.ArrayList
 
 class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
 
@@ -29,15 +27,17 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
 
     fun addItem(item : String) {
         if (!TextUtils.isEmpty(item)) {
-            mData.add(createItem(item))
+            mData.add(MessageItem(item, Utils.randomColor()))
             notifyDataSetChanged()
         }
     }
 
     fun addItems(items : List<String>) {
         if (items.isNotEmpty()) {
-            for (item in items) {
-                mData.add(createItem(item))
+            var color = Utils.randomColor()
+
+            for (i in items.size-1 downTo  0) {
+                mData.add(0, MessageItem(items[i], color))
             }
 
             notifyDataSetChanged()
@@ -48,12 +48,6 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
         holder.itemView.tvMessage.text = mData.get(position).text
         val colorFilter = PorterDuffColorFilter(mData.get(position).color!!, PorterDuff.Mode.MULTIPLY)
         holder.itemView.background.colorFilter = colorFilter
-    }
-
-    private fun createItem(s : String, color: Color) : MessageItem {
-        val item = MessageItem(s, Utils.randomColor())
-
-        return item
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
--- app/src/main/java/com/example/tweeter/ui/message/MessagesFragment.kt
+++ app/src/main/java/com/example/tweeter/ui/message/MessagesFragment.kt
@@ -1,16 +1,21 @@
 package com.example.tweeter.ui.message
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import com.example.tweeter.R
 import com.example.tweeter.di.component.AppComponent
 import com.example.tweeter.presenter.MessagesPresenter
 import com.example.tweeter.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.dialog_input.view.*
 import kotlinx.android.synthetic.main.fragment_messages.*
 import javax.inject.Inject
 
@@ -29,6 +34,8 @@ class MessagesFragment : BaseFragment(), IMessageView{
     }
 
     private lateinit var mAdapter : MessagesAdapter
+
+    private  var mDialog : Dialog ?= null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -52,7 +59,6 @@ class MessagesFragment : BaseFragment(), IMessageView{
     private fun setupUI() {
 
         mAdapter = MessagesAdapter()
-        mAdapter.addItems(mutableListOf("message 1", "message 2", "message 3"))
 
         rvMessages.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
@@ -60,10 +66,103 @@ class MessagesFragment : BaseFragment(), IMessageView{
             this.adapter = mAdapter
         }
 
-//        mMessagesPresenter.processMessages(etInputMessage.text.toString())
-//        etInputMessage.text = null
+        fab.setOnClickListener({
+            showDialog()
+        })
 
 
+
+    }
+
+    private fun showDialog() {
+        if (mDialog == null) {
+            mDialog = createDialog()
+        }
+
+        mDialog?.show()
+    }
+
+    private fun createDialog() : Dialog {
+
+        val dialogView = View.inflate(activity, R.layout.dialog_input, null)
+
+        val dialog = Dialog(activity, R.style.DialogStyle)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(dialogView)
+
+        dialogView.ivClose.setOnClickListener {
+            revealShow(dialogView, false, dialog)
+        }
+
+        dialog.setOnShowListener {
+            revealShow(dialogView, true, null)
+        }
+
+        dialog.setOnKeyListener { dialogInterface, i, keyEvent ->
+
+            if (i == KeyEvent.KEYCODE_BACK) {
+                revealShow(dialogView, false, dialog)
+                true
+            }
+            false
+
+        }
+
+        dialog.window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        // handle post button
+        dialogView.btPost.setOnClickListener {
+            mMessagesPresenter.processMessages(dialogView.etInputMessage.text.toString())
+            dialogView.etInputMessage.text = null
+            revealShow(dialogView, false, dialog)
+        }
+
+        return dialog
+    }
+
+    private fun revealShow(dialogView : View, b : Boolean, dialog: Dialog?) {
+        val view = dialogView.dialog
+
+        val w = view.width
+        val h = view.height
+
+        val endRadius = Math.hypot(w.toDouble(), h.toDouble())
+
+        val cx : Int = (fab.x + fab.width/2).toInt()
+        val cy : Int = (fab.y + fab.height + 56).toInt()
+
+        if (b) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val revealAnimator = ViewAnimationUtils.createCircularReveal(view, cx, cy, 0f, endRadius.toFloat())
+
+                view.visibility = View.VISIBLE
+                revealAnimator.duration = 400
+                revealAnimator.start()
+            }
+            else {
+                view.visibility = View.VISIBLE
+            }
+        }
+        else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val animator = ViewAnimationUtils.createCircularReveal(view, cx, cy, endRadius.toFloat(), 0f)
+                animator.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        dialog?.dismiss()
+                        view.visibility = View.INVISIBLE
+                    }
+                })
+
+                animator.duration = 200
+                animator.start()
+            }
+            else {
+                dialog?.dismiss()
+                view.visibility = View.INVISIBLE
+            }
+        }
     }
 
     override fun addMessages(messages: List<String>) {
